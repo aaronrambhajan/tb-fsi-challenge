627d8b2d92bdb2d44f61f40bd9e458fda73d71b4
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2019-02-12 14:34:36 -0500

    basic testing for prime calculation

---
+++ api/calculatePrimes.test.js
@@ -0,0 +1,9 @@
+import calculatePrimes from '../api/calculatePrimes';
+
+test('Calculates primes for 10 to equal [3, 5]', () => {
+  expect(calculatePrimes(10)).toEqual([3, 5]);
+});
+
+test('Calculates primes for 18 to equal [7]', () => {
+  expect(calculatePrimes(18)).toEqual([7]);
+});
